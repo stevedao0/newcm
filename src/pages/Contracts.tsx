a602af6725d558b6439a9dd89711a45618decc34
stevedao0 <114713382+stevedao0@users.noreply.github.com>
2025-07-30 22:24:11 +0700

    Fix date handling errors in contract pages

--- src/pages/Contracts.tsx
+++ src/pages/Contracts.tsx
@@ -976,7 +976,7 @@ const Contracts: React.FC = () => {
                   type="date"
                   name="ngayKy"
                   className="w-full px-3 py-2 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                  defaultValue={selectedContract?.ngayKy ? selectedContract.ngayKy.split('/').reverse().join('-') : ''}
+                  defaultValue={selectedContract?.ngayKy ? formatDate(selectedContract.ngayKy).split('/').reverse().join('-') : ''}
                   required
                 />
               </div>
